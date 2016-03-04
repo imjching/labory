6ae78a4024470e0eb01b1c6f0ca5aa767e0eb07d
Jay <imjching@hotmail.com>
2016-03-04 16:30:51 +0800

    Whitelist the whole private network for web console in dev mode

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,9 @@ module Labory
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    if Rails.env.development?
+      config.web_console.whitelisted_ips = '192.168.0.0/16'
+    end
   end
 end
