28f092eeaefde9b4e28d053029c884d39a3c32ea
Jay <imjching@hotmail.com>
2016-04-01 01:49:34 +0800

    Modules can now be added/removed from classrooms

--- app/controllers/stafftools/classrooms_controller.rb
+++ app/controllers/stafftools/classrooms_controller.rb
@@ -2,7 +2,7 @@ module Stafftools
   class ClassroomsController < StafftoolsController
     layout :resolve_layout
 
-    before_action :set_classroom, except: [:index, :new, :create]
+    before_action :set_classroom, except: [:index, :new, :create, :edit_modules, :update_modules]
 
     def index
       @classrooms = current_user.classrooms.page(params[:page])
@@ -16,7 +16,7 @@ module Stafftools
       @classroom = Classroom.new(new_classroom_params)
 
       if @classroom.save
-        redirect_to stafftools_classroom_path(@classroom)
+        redirect_to modules_settings_stafftools_classroom_path(@classroom)
       else
         render :new
       end
@@ -35,7 +35,28 @@ module Stafftools
     end
 
     def show_modules
+      @courses = @classroom.classrooms_courses.includes(:course).rank(:sort_order)
+    end
 
+    def edit_modules
+      @classroom = current_user.classrooms.includes(:courses).find_by!(slug: params[:id])
+      @available_courses = current_user.courses
+    end
+
+    def update_modules
+      @classroom = current_user.classrooms.includes(:courses).find_by!(slug: params[:id])
+      @available_course_ids = current_user.courses.pluck(:id)
+
+      # Check if the input course_ids are in @available_course_ids
+      filtered = update_modules_params
+      filtered[:course_ids].select! { |x| @available_course_ids.include? x.to_i }
+
+      if @classroom.update_attributes(filtered)
+        flash[:success] = "Updated classroom!"
+        redirect_to modules_settings_stafftools_classroom_path(@classroom)
+      else
+        render :edit_modules
+      end
     end
 
     def show_graphs
@@ -53,6 +74,12 @@ module Stafftools
         .require(:classroom)
         .permit(:title)
         .merge(users: [current_user])
+    end
+
+    def update_modules_params
+      params
+        .require(:classroom)
+        .permit( { course_ids: [] } )
     end
 
     def resolve_layout
--- app/models/classrooms_course.rb
+++ app/models/classrooms_course.rb
@@ -1,11 +1,19 @@
 class ClassroomsCourse < ActiveRecord::Base
 
-  include RankedModel
+  before_save :set_sort_order
+  include RankedModel # this must come after set_sort_order
+  ranks :sort_order, with_same: :classroom_id
 
   belongs_to :classroom
   belongs_to :course
 
-  ranks :sort_order, with_same: :classroom_id
+  private
+
+  def set_sort_order
+    if self.new_record?
+      self.sort_order_position = :last
+    end
+  end
 
 end
 
