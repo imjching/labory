b4facfb5511812fadee94af5888161d32dbaa552
Jay <jay@imjching.com>
2016-04-23 22:42:46 +0800

    Minor changes

--- app/controllers/classrooms_controller.rb
+++ app/controllers/classrooms_controller.rb
@@ -4,12 +4,6 @@ class ClassroomsController < ApplicationController
     @classroom = current_user.classrooms.find_by!(slug: params[:id])
     @courses_count = @classroom.classrooms_courses.rank(:sort_order).where(visible: true).count
     @classrooms_courses = @classroom.classrooms_courses.rank(:sort_order).where(visible: true).includes(course: [:labs]).page(params[:page]).per(5)
-
-    # p @classrooms_courses
-    # if @courses.empty?
-    #   p "hello"
-    # end
-
   end
 
 end
--- app/controllers/solutions_controller.rb
+++ app/controllers/solutions_controller.rb
@@ -52,16 +52,4 @@ class SolutionsController < ApplicationController
 
   end
 
-  # def show
-  #   @classroom = current_user.classrooms.find_by!(slug: params[:id])
-  #   @courses_count = @classroom.classrooms_courses.rank(:sort_order).where(visible: true).count
-  #   @classrooms_courses = @classroom.classrooms_courses.rank(:sort_order).where(visible: true).includes(course: [:labs]).page(params[:page]).per(5)
-
-  #   # p @classrooms_courses
-  #   # if @courses.empty?
-  #   #   p "hello"
-  #   # end
-
-  # end
-
 end
